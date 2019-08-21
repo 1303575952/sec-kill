c5481d492256821f2b75fcbee2d5ec908fa62991
1303575952 <1303575952@qq.com>
2019-08-21 12:47:13 +0800

    下订单接口sessionid对应的uid和传参的uid不同则返回403

--- src/main/java/com/doudizu/seckill/controller/OrderController.java
+++ src/main/java/com/doudizu/seckill/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.doudizu.seckill.conf.PropertiesConf;
 import com.doudizu.seckill.domain.Order;
 import com.doudizu.seckill.domain.Product;
+import com.doudizu.seckill.redis.OrderKey;
 import com.doudizu.seckill.redis.RedisService;
 import com.doudizu.seckill.service.OrderService;
 import com.doudizu.seckill.service.ProductService;
@@ -17,6 +18,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,27 +47,42 @@ public class OrderController {
         return new ResponseEntity<>(returnMap, HttpStatus.OK);
     }
 
-    //下订单接口
     @RequestMapping(value = "/order", method = RequestMethod.POST)
     @ResponseBody
-    public ResponseEntity<Map> createOrder(@RequestBody Map<String, String> map) {
+    public ResponseEntity<Map> createOrder(HttpServletRequest request, @RequestBody Map<String, String> map) {
         Map<String, Object> returnMap = new HashMap<>();
+        log.info(request.getQueryString());
         int uid = Integer.valueOf(map.get("uid"));
         int pid = Integer.valueOf(map.get("pid"));
+        String sessionid = request.getHeader("sessionid");
+        log.info("sessionid:" + sessionid);
+        //拿到redis上sessionid对应的uid
+        int requestUid = Integer.valueOf(redisService.getKey(OrderKey.getByOrderId, sessionid));
+        log.info("requestUid:" + requestUid);
+        //判断请求的uid和参数中uid是否一致
+        //请求的uid和参数中uid不一致
+        if (requestUid != uid) {
+            return new ResponseEntity<>(returnMap, HttpStatus.FORBIDDEN);
+        }
+        //请求的uid和参数中uid一致
         //判断库存
         Product product = productService.getProductByPid(pid);
         int stock = product.getCount();
         if (stock <= 0) {
+            log.info("商品" + pid + "库存不够，不可下单");
             returnMap.put("code", 1);
             return new ResponseEntity<>(returnMap, HttpStatus.OK);
         }
         //判断是否可秒杀（已购买过）
         List<Order> orders = orderService.getOrderByUidAndPid(uid, pid);
         if (orders.size() >= 1) {
+            log.info("商品" + pid + "用户" + uid + "已经购买过");
             returnMap.put("code", 1);
+            log.info(JSONObject.toJSON(returnMap).toString());
             return new ResponseEntity<>(returnMap, HttpStatus.OK);
         }
         //减库存 下订单 写入秒杀订单
+        log.info("商品" + pid + "用户" + uid + "可下单");
         String orderId = orderService.createOrder(uid, pid);
         log.info("orderId:" + orderId);
         returnMap.put("code", 0);
