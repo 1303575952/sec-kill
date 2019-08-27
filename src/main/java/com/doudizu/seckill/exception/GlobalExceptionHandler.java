2a99df75dbab5491d5e407a942dfda59ffec1e7a
1303575952 <1303575952@qq.com>
2019-08-27 10:31:15 +0800

    未能处理的异常统一返回403

--- src/main/java/com/doudizu/seckill/exception/GlobalExceptionHandler.java
+++ src/main/java/com/doudizu/seckill/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.doudizu.seckill.exception;
 
 import com.doudizu.seckill.result.CodeMsg;
 import com.doudizu.seckill.result.Result;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,20 +16,24 @@ import java.util.List;
 @ControllerAdvice
 @ResponseBody
 public class GlobalExceptionHandler {
-	@ExceptionHandler(value=Exception.class)
-	public Result<String> exceptionHandler(HttpServletRequest request, Exception e){
-		e.printStackTrace();
-		if(e instanceof GlobalException) {
-			GlobalException ex = (GlobalException)e;
-			return Result.error(ex.getCm());
-		}else if(e instanceof BindException) {
-			BindException ex = (BindException)e;
-			List<ObjectError> errors = ex.getAllErrors();
-			ObjectError error = errors.get(0);
-			String msg = error.getDefaultMessage();
-			return Result.error(CodeMsg.BIND_ERROR.fillArgs(msg));
-		}else {
-			return Result.error(CodeMsg.SERVER_ERROR);
-		}
-	}
+    /*@ExceptionHandler(value=Exception.class)
+    public Result<String> exceptionHandler(HttpServletRequest request, Exception e){
+        e.printStackTrace();
+        if(e instanceof GlobalException) {
+            GlobalException ex = (GlobalException)e;
+            return Result.error(ex.getCm());
+        }else if(e instanceof BindException) {
+            BindException ex = (BindException)e;
+            List<ObjectError> errors = ex.getAllErrors();
+            ObjectError error = errors.get(0);
+            String msg = error.getDefaultMessage();
+            return Result.error(CodeMsg.BIND_ERROR.fillArgs(msg));
+        }else {
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }
+    }*/
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity exceptionHandler(HttpServletRequest request, Exception e) {
+        return new ResponseEntity<>(null, HttpStatus.FORBIDDEN);
+    }
 }
