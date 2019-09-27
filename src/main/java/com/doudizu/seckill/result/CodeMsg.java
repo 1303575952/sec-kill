f652d95cd07a9dc1f9ec77763a4c86cd417339fc
1303575952 <1303575952@qq.com>
2019-09-27 09:10:25 +0800

    改为单机版

--- src/main/java/com/doudizu/seckill/result/CodeMsg.java
+++ src/main/java/com/doudizu/seckill/result/CodeMsg.java
@@ -12,17 +12,11 @@ public class CodeMsg {
 	private int code;
 	private String msg;
 	
-	//通用的错误码
+	//通用的错误码，这里暂未使用
 	public static CodeMsg SUCCESS = new CodeMsg(0, "success");
 	public static CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务端异常");
 	public static CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
-	
-	//商品模块 5003XX
-	
-	//订单模块 5004XX
-	
-	//秒杀模块 5005XX
-	
+
 	private CodeMsg( ) {
 	}
 			
